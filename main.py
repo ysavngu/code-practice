767bcbfd9fa0e254cb22fe8310321a8a029d82a2
ysavngu <fuzzy.wormzs@gmail.com>
2025-03-20 19:22:34 -0400

    addition and subtraction function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("Hello World")
+def addition (num1: int, num2: int):
+    total_num = num1 + num2
+    return total_num
+def subtraction(num1: int, num2: int):
+    total_num = num1 - num2
+    return total_num
+total = addition(5,1)
+print(total)
+minus = subtraction(9,4)
+print(minus)
